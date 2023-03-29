1c6d4bea28a656c4c97973509b5ec3c019530f26
zcaspall <55821382+zcaspall@users.noreply.github.com>
2023-03-29 01:07:40 -0500

    Add search bar and login page layouts

--- app/src/main/java/com/example/local_venues_frontend/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/local_venues_frontend/ui/profile/ProfileScreen.kt
@@ -1,8 +1,9 @@
 package com.example.local_venues_frontend.ui.profile
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun ProfileScreen() {
-
+    Text(text = "PROFILE SCREEN")
 }
