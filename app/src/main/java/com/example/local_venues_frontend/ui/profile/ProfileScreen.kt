09a0c64b4c115a61c5184ecb18a7bdd49b290c04
xaviercharlemagne <xaviercharlemagne728@gmail.com>
2023-04-14 19:50:36 -0500

    Just saving to the branch to update all the code. Still working on a scrollable feed but making headway

--- app/src/main/java/com/example/local_venues_frontend/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/local_venues_frontend/ui/profile/ProfileScreen.kt
@@ -1,31 +1,29 @@
 package com.example.local_venues_frontend.ui.profile
 
-import android.content.ClipData
-import android.graphics.PostProcessor
-import android.media.Image
+// import android.content.ClipData
+// import android.graphics.PostProcessor
+// import android.media.Image
 import android.os.Bundle
-import android.os.Message
+// import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+// import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.draggable
+// import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
+// import androidx.compose.foundation.lazy.LazyColumn
+// import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.BasicTextField
+// import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.ShoppingCart
+// import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 
@@ -34,39 +32,44 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.semantics.Role.Companion.Image
+// import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.local_venues_frontend.LocalVenuesApp
+// import androidx.core.view.ScrollingView
+// import com.example.local_venues_frontend.LocalVenuesApp
 import com.example.local_venues_frontend.R
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ScrollableState()
+            // Figure out why this isnt an accepted float
+            // ScrollingScreen()
         }
     }
 }
 
+// Feed is not showing up
+
 @Preview
 @Composable
-fun Feed(){
-    Column(){
-        Image(bitmap = ImageBitmap.imageResource(id = R.drawable.profile_picture))
+fun Feed(modifier: Modifier = Modifier){
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .offset(x = 0.dp, y = 200.dp)
+        , horizontalAlignment = Alignment.CenterHorizontally){
+        Image(bitmap = ImageBitmap.imageResource(id = R.drawable.profile_picture), contentDescription = "feed")
+        Image(bitmap = ImageBitmap.imageResource(id = R.drawable.profile_picture1), contentDescription = "feed")
+        Image(bitmap = ImageBitmap.imageResource(id = R.drawable.gun_lady), contentDescription = "feed")
     }
 }
 
 @Preview
 @Composable
 fun ProfileScreen() {
-//    var value by remember{
-//        mutableStateOf("")
-//    }
-
-    val listState = rememberLazyListState()
 
     Box(modifier = Modifier
         .background(Color.LightGray)
@@ -106,17 +109,4 @@ fun ProfileScreen() {
             Text("This is about where the bio would go once we hook up the backend")
         }
     }
-
-    LazyColumn(state = listState, horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxWidth(1F)
-            .offset(x = 0.dp, y = 200.dp)
-            .padding(8.dp)){
-
-        item(){
-            Text("wow this stuff is really a lot to figure out", textAlign = TextAlign.Center)
-        }
-
-    }
-
 }
