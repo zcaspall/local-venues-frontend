97293cf064fa935208becb827951d91f8ba26b32
xaviercharlemagne <xaviercharlemagne728@gmail.com>
2023-05-01 08:16:12 -0500

    Completed the user page

--- app/src/main/java/com/example/local_venues_frontend/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/local_venues_frontend/ui/profile/ProfileScreen.kt
@@ -1,13 +1,21 @@
 package com.example.local_venues_frontend.ui.profile
 
+
 // import android.content.ClipData
 // import android.graphics.PostProcessor
 // import android.media.Image
 //import android.content.ClipData
+
+
+import android.content.ClipData
+import android.graphics.PostProcessor
+import android.media.Image
+
 import android.os.Bundle
-// import android.os.Message
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 // import androidx.compose.animation.ExperimentalAnimationApi
@@ -17,35 +25,61 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 // import androidx.compose.foundation.lazy.LazyRow
 //import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
 
 import androidx.compose.foundation.shape.CircleShape
-// import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.rounded.ArrowBack
+
 import androidx.compose.material.icons.rounded.Person
 // import androidx.compose.material.icons.rounded.ShoppingCart
+
+import androidx.compose.material.icons.rounded.ShoppingCart
+
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 // import androidx.compose.ui.semantics.Role.Companion.Image
+
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 import androidx.compose.ui.unit.sp
 // import androidx.core.view.ScrollingView
 // import com.example.local_venues_frontend.LocalVenuesApp
+
+import androidx.core.view.ScrollingView
+import com.example.local_venues_frontend.LocalVenuesApp
+
 import com.example.local_venues_frontend.R
 
 
